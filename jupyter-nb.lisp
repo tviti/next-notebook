247d1ae0ed953d3dd7cc7bde6626fb9763a13534
tviti <tviti@hawaii.edu>
2019-10-07 18:01:49 -1000

    Split Emacs string editing into its own defun.

--- jupyter-nb.lisp
+++ jupyter-nb.lisp
@@ -29,38 +29,44 @@ Jupyter.notebook.focus_cell();")))
        "k" #'select-prev-cell)
      (list :vi-normal vi-map)))))
 
+(defun edit-str-with-emacs (str tempfile)
+  "Dump the contents of str to the temporary file tempfile, then open tempfile
+in Emacs for editing. Note that this call is synchronous!"
+  ;; Dump the cell's contents to a tempfile
+  (with-open-file (s tempfile :direction :output :if-exists :supersede)
+    ;; Replace \n with literal newlines
+    (format s "~a" (ppcre:regex-replace-all "\\\\n" str "
+")))
+  ;; Open an emacs buffer pointed at the file
+  (let* ((shell-cmd `("emacsclient" ,tempfile)))
+    (uiop:run-program shell-cmd :output :string))
+  ;; Read the file's contents to the browser buffer's active input field.
+  (with-open-file (s tempfile :direction :input)
+    (let ((contents (make-string (file-length s)))
+	  (output nil))
+      (read-sequence contents s)
+      contents)))
+
 (defun edit-cell-callback (js-result)
-  ;; TODO: Decode the cell as a JSON object instead of a custom array
-  ;; use regexp to split the result
+  ;; TODO: Communicate the cell as a JSON object instead of a custom array
   (ppcre:register-groups-bind (cell-type cell-contents)
       ("\\['(.*?)',.*?['\"](.*?)['\"]\\]" js-result)
     (let ((tempfile (format nil "/tmp/next-tmp.~a"
 			    ;; Use the right file extension 
 			    (cond ((string= cell-type "markdown") "md")
 				  ((string= cell-type "code") "py")
-				  (t ".txt")))))
-      ;; Dump the cell's contents to a tempfile
-      (with-open-file (s tempfile :direction :output :if-exists :supersede)
-	;; Replace \n with literal newlines
-	(format s "~a" (ppcre:regex-replace-all "\\\\n" cell-contents "
-")))
-      ;; Open an emacs buffer pointed at the file
-      (let* ((shell-cmd `("emacsclient" ,tempfile)))
-	(uiop:run-program shell-cmd :output :string))
-      ;; Read the file's contents to the browser buffer's active input field.
-      (with-open-file (s tempfile :direction :input)
-	(let ((contents (make-string (file-length s)))
-	      (output nil))
-	  (read-sequence contents s)
-	  ;; Escape backticks (since those are JS multiline string char).
-	  (setq output (ppcre:regex-replace-all "`" contents "\\\\`"))
-	  (rpc-buffer-evaluate-javascript
-	   (current-buffer)
-	   (format nil "
+				  (t ".txt"))))
+	  (contents nil))
+      (setq contents (edit-str-with-emacs cell-contents tempfile))
+      ;; Escape backticks (since those are JS multiline string char).
+      (setq output (ppcre:regex-replace-all "`" contents "\\\\`"))
+      (rpc-buffer-evaluate-javascript
+       (current-buffer)
+       (format nil "
 (function () {
     var cell = Jupyter.notebook.get_selected_cell();
     cell.set_text(`~a`);
-})();" output)))))))
+})();" output)))))
 
 (define-command edit-cell ()
   "Open a new emacs frame using the `emacsclient' mechanism, and place the value
