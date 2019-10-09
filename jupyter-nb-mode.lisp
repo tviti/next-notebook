124acf30fc74a4ceba5b1905537bccffb966c0bf
Taylor Viti <tviti@hawaii.edu>
2019-10-08 23:21:00 -1000

    Start using parenscript for javascript.

--- jupyter-nb-mode.lisp
+++ jupyter-nb-mode.lisp
@@ -1,99 +1,102 @@
 (uiop:define-package :next/jupyter-nb-mode
-    (:use :next :common-lisp)
+    (:use :next :common-lisp :ps)
     (:documentation "Mode for interacting with Jupyter notebooks."))
 (in-package :next/jupyter-nb-mode)
 
 ;; TODO: I think a macro might be nice for all of these glue-calls to JS.
 ;; That or lean on parenscript for doing this.
 
-(define-command select-next-cell ()
+(ps:defpsmacro %nb-chain (&rest args)
+  `(ps:chain *jupyter notebook ,@args))
+
+(ps:defpsmacro %get-nb ()
+  '(%nb-chain))
+
+;; TODO: These could be implemented as closures of a single function called
+;; something like `select-cell-relative' (that does relative cell motion).
+(define-command select-next-cell (&optional (buffer (current-buffer)))
   "Move one cell upwards, also moving the buffer's focus."
   (rpc-buffer-evaluate-javascript
-   (current-buffer)
-   (format nil "
-Jupyter.notebook.select_next(true);
-Jupyter.notebook.focus_cell();")))
+   buffer
+   (ps:ps (%nb-chain (select_next t))
+	  (%nb-chain (focus_cell)))))
 
-(define-command select-prev-cell ()
+(define-command select-prev-cell (&optional (buffer (current-buffer)))
   "Move one cell downwards, also moving the buffer's focus."
   (rpc-buffer-evaluate-javascript
-   (current-buffer)
-   (format nil "
-Jupyter.notebook.select_prev(true);
-Jupyter.notebook.focus_cell();")))
+   buffer
+   (ps:ps (%nb-chain (select_prev t))
+	  (%nb-chain (focus_cell)))))
 
-(define-command select-cell (idx)
+(define-command select-cell (idx &optional (buffer (current-buffer)))
   "Select cell idx, also moving the buffer's focus."
   (rpc-buffer-evaluate-javascript
-   (current-buffer)
-   (format nil "
-Jupyter.notebook.select(~d, true);
-Jupyter.notebook.focus_cell();" idx)))
+   buffer
+   (ps:ps (%nb-chain (select (ps:lisp idx) t))
+	  (%nb-call-method focus_cell))))
 
-(define-command select-first-cell ()
-  (select-cell 0))
+(define-command select-first-cell (&optional (buffer (current-buffer)))
+  (select-cell 0 buffer))
 
-(define-command select-last-cell ()
+(define-command select-last-cell (&optional (buffer (current-buffer)))
   (rpc-buffer-evaluate-javascript
-   (current-buffer)
-   "
-(function () {
-    var nb = Jupyter.notebook;
-    var cells = nb.get_cells();
-    nb.select(cells.length - 1);
-    nb.focus_cell();
-})();"))
+   buffer
+   (ps:ps (let* ((n-cells (ps:@ (%nb-call-method get_cells) length)))
+	    (%nb-chain (select (- n-cells 1)))
+	    (%nb-chain (focus_cell))))))
 
-(define-command execute-selected-cells ()
+(define-command execute-selected-cells (&optional (buffer (current-buffer)))
   "Run the currently selected cell(s)."
   (rpc-buffer-evaluate-javascript
-   (current-buffer)
-   "
-Jupyter.notebook.execute_selected_cells();"))
+   buffer
+   (ps:ps (%nb-chain (execute_selected_cells)))))
 
-(define-command execute-all-cells ()
+(define-command execute-all-cells (&optional (buffer (current-buffer)))
   "Run the entire notebook."
   (rpc-buffer-evaluate-javascript
-   (current-buffer)
-   "
-Jupyter.notebook.execute_all_cells();"))
+   buffer
+   (ps:ps (%nb-chain (execute_all_cells)))))
 
-(define-command edit-cell ()
+(define-command edit-cell (&optional (buffer (current-buffer)))
   "Open the selected cell's source in an emacs buffer for editing, using a
 temporary file (whose location is currently hardcoded). The temporary files
 extension is chosen based on the cell's type, with code cells given a `.py`
 extension, markdown cells a `.md`, and all others `.txt`. Thus, emacs should
 drop you into the appropriate mode when the buffer is created."
-  (request-cell-data #'edit-cell-callback))
+  ;; TODO: Change this to `request-cell-contents'
+  (request-cell-data (lambda (retval)
+		       (edit-cell-callback retval buffer))))
 
-(define-command save-notebook ()
+(define-command save-notebook (&optional (buffer (current-buffer)))
   "Save the currently active notebook."
   (rpc-buffer-evaluate-javascript
-   (current-buffer)
-   "
-Jupyter.notebook.save_notebook();"))
+   buffer
+   (ps:ps (%nb-chain (save_notebook)))))
 
-(define-command save-checkpoint ()
+(define-command save-checkpoint (&optional (buffer (current-buffer)))
   "Create a checkpoint (not sure; I don't manually do this so I'm not positive
 what constitutes a checkpoint)."
   (rpc-buffer-evaluate-javascript
-   (current-buffer)
-   "
-Jupyter.notebook.save_checkpoint();"))
+   buffer
+   (ps:ps (%nb-chain (save_checkpoint)))))
 
-(define-command copy-cell ()
+(define-command copy-cell (&optional (buffer (current-buffer)))
   "Copy the selected cell(s)."
   (rpc-buffer-evaluate-javascript
-   (current-buffer)
-   "
-Juptery.notebook.copy_cell();"))
+   buffer
+   (ps:ps (%nb-chain (copy_cell)))))
 
-(define-command scroll-some (ammt)
+(define-command scroll-some (ammt &optional (buffer (current-buffer)))
   "Scroll the page by ammt using the notebooks scroll_manager."
   (rpc-buffer-evaluate-javascript
-   (current-buffer)
-   (format nil "
-Jupyter.notebook.scroll_manager.scroll_some(~f);" ammt)))
+   buffer
+   (ps:ps (%nb-chain scroll_manager (scroll_some (ps:lisp ammt))))))
+
+(define-command scroll (ammt &optional (buffer (current-buffer)))
+  "Scroll the page by ammt using the notebooks scroll_manager."
+  (rpc-buffer-evaluate-javascript
+   buffer
+   (ps:ps (%nb-chain scroll_manager (scroll (ps:lisp ammt))))))
 
 (define-mode jupyter-nb-mode ()
   "A mode for interacting with Jupyter notebooks, with facilities for editing
@@ -109,30 +112,28 @@ the notebook's contents using the emacsclient mechanism."
        "e" #'edit-cell
        "j" #'select-next-cell
        "k" #'select-prev-cell
-       "C-e" (lambda () (scroll-some 0.25))
-       "C-y" (lambda () (scroll-some -0.25)))
+       "C-e" (lambda () (scroll-some 0.1))
+       "C-y" (lambda () (scroll-some -0.1)))
      (list :vi-normal vi-map)))))
 
+;; TODO: This is copy pasta w/ my init.lisp! (break off into a package?)
 (defun edit-str-with-emacs (str tempfile)
   "Dump the contents of str to the temporary file tempfile, then open tempfile
 in Emacs for editing. Note that this call is synchronous!"
-  ;; TODO: This is copy pasta w/ my init.lisp! (break off into a package?)
   ;; Dump the cell's contents to a tempfile
   (with-open-file (s tempfile :direction :output :if-exists :supersede)
     ;; Replace \n with literal newlines
-    (format s "~a" (ppcre:regex-replace-all "\\\\n" str "
-")))
+    (format s "~a" str))
   ;; Open an emacs buffer pointed at the file
   (let* ((shell-cmd `("emacsclient" ,tempfile)))
     (uiop:run-program shell-cmd :output :string))
-  ;; Read the file's contents to the browser buffer's active input field.
+  ;; Read the file contents back in
   (with-open-file (s tempfile :direction :input)
     (let ((contents (make-string (file-length s))))
       (read-sequence contents s)
-      ;; Escape backticks (since those are JS multiline string char).
-      (ppcre:regex-replace-all "`" contents "\\\\`"))))
+      contents)))
 
-(defun edit-cell-callback (js-result)
+(defun edit-cell-callback (js-result buffer)
   (let* ((json-result (cl-json:decode-json-from-string js-result))
 	 (cell--type (rest (assoc :cell--type json-result)))
 	 (source (rest (assoc :source json-result)))
@@ -141,13 +142,10 @@ in Emacs for editing. Note that this call is synchronous!"
 			   (cond ((string= cell--type "markdown") "md")
 				 ((string= cell--type "code") "py")
 				 (t ".txt"))))
-	 (output nil))
-    (setq output (edit-str-with-emacs source tempfile))
-    (rpc-buffer-evaluate-javascript (current-buffer) (format nil "
-(function () {
-    var cell = Jupyter.notebook.get_selected_cell();
-    cell.set_text(`~a`);
-})();" output))))
+	 (output (edit-str-with-emacs source tempfile)))
+    (rpc-buffer-evaluate-javascript
+     buffer
+     (ps:ps (%nb-chain (get_selected_cell) (set_text (ps:lisp output)))))))
 
 (defun request-cell-data (callback)
   "Request the cell type, source, and metadata."
